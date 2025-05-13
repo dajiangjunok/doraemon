d97bd6f10293ee57e64a3d38fd9bff7d0cbeb524
Alivin <wangyongjie@mediinfo.com.cn>
2025-05-13 11:15:16 +0800

    feat: create face

--- src/pages/Doraemon.jsx
+++ src/pages/Doraemon.jsx
@@ -13,18 +13,18 @@ const Doraemon = () => {
         className="header-container
         absolute
         mt-3
-      w-[350px] 
-      h-[320px]
+        w-[350px] 
+        h-[320px]
        bg-primary 
-       border-6 
+        border-6 
        border-black
-       rounded-full
+        rounded-full
         rotate-[-25deg]
         overflow-hidden
        "
       >
         <div
-          className="absolute
+          className="absolute 
           top-[96px]
           left-[50px]
           header-face-container
@@ -40,7 +40,7 @@ const Doraemon = () => {
           "
         >
           <div
-            className="left-eye  
+            className="left-eye  z-2
             absolute
             left-[84px]
             top-[-28px]
@@ -55,8 +55,8 @@ const Doraemon = () => {
           >
             <div className="absolute bottom-[10px] right-[2px] w-[16px] h-[16px] bg-black rounded-full"></div>
           </div>
-          <div
-            className="right-eye
+          <div 
+            className="right-eye z-2
           left-eye  
             absolute
             left-[140px]
@@ -74,7 +74,7 @@ const Doraemon = () => {
             <div className="absolute bottom-[4px] right-[0px] w-[16px] h-[16px] bg-black rounded-full"></div>
           </div>
           <div
-            className="nose absolute w-[40px] h-[40px] border-6 
+            className="nose absolute w-[40px] h-[40px] border-6 z-2
           border-black 
           bg-[#c5432b]
           rounded-full
@@ -88,6 +88,12 @@ const Doraemon = () => {
             "
             ></div>
           </div>
+          {/* face line */}
+          <div className="absolute border-6  border-transparent z-1
+       border-r-black  w-[400px] h-[400px] rounded-full rotate-[16deg] top-[-50px] left-[-214px]"></div>
+          {/* mouth */}
+          <div className="absolute border-6 border-transparent border-r-black z-1 w-[210px] h-[238px] rounded-full top-[-40px] left-[36px] rotate-[64deg]"></div>
+          <div className="absolute w-[70px] h-[30px] bg-white z-2 left-[150px] top-[176px] rotate-[-22deg]"></div>
         </div>
       </div>
     </div>
